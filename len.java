d28fbcc5cb47ab14752ace55e71f62a8f04a847a
Vaishnavi vitthal Khedkar <166608890+Vaishhkhedkar@users.noreply.github.com>
2025-01-26 21:53:47 +0530

    length method
    class len 
    {
        public static void main(String args[])
        {
            String s="vaishnavi vikas jaybhaye";
            int l=s.length();
            System.out.println("length="+l); 
        }
    }

---
+++ len.java
@@ -0,0 +1,9 @@
+class len 
+{
+    public static void main(String args[])
+    {
+        String s="vaishnavi vikas jaybhaye";
+        int l=s.length();
+        System.out.println("length="+l); 
+    }
+}
