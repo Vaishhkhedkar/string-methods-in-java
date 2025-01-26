8bfbb4fb3d323dad653cd8aa74a47035c3f3b19a
Vaishnavi vitthal Khedkar <166608890+Vaishhkhedkar@users.noreply.github.com>
2025-01-26 22:00:53 +0530

    toLoweCase & toUpperCase
    class locase
    {
        public static void main(String args[])
        {
            String S="vaishnavi vikas jaybhaye";
        
            System.out.println(S.toLowerCase()); 
            System.out.println(S.toUpperCase()); 
        }
    }

---
+++ locase.java
@@ -0,0 +1,10 @@
+class locase
+{
+    public static void main(String args[])
+    {
+        String S="vaishnavi vikas jaybhaye";
+    
+        System.out.println(S.toLowerCase()); 
+        System.out.println(S.toUpperCase()); 
+    }
+}
