4dfab3bb0a3d57b44d37fc9d81d4d79b4ec0e1d6
Vaishnavi vitthal Khedkar <166608890+Vaishhkhedkar@users.noreply.github.com>
2025-01-27 18:26:53 +0530

    concat method
    class conca
    {
        public static void main (String args [])
        {
            String s="Welcome to Home";
            String s1="and office";
            System.out.println(s.concat (s1));
        }
    }

---
+++ conca.java
@@ -0,0 +1,9 @@
+class conca
+{
+    public static void main (String args [])
+    {
+        String s="Welcome to Home";
+        String s1="and office";
+        System.out.println(s.concat (s1));
+    }
+}
