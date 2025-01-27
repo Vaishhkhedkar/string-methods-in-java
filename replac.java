8ce1097b877ac78008818e709cc2272b00b5e844
Vaishnavi vitthal Khedkar <166608890+Vaishhkhedkar@users.noreply.github.com>
2025-01-27 18:33:32 +0530

    replace method
    class replac
    {
    
        public static void main (String args [])
        {
            String s="Welcome to Home";
        System.out.println(s.replace ("to","two"));
            System.out.println(s.replace ("o","v"));
        }
    }

---
+++ replac.java
@@ -0,0 +1,10 @@
+class replac
+{
+
+    public static void main (String args [])
+    {
+        String s="Welcome to Home";
+    System.out.println(s.replace ("to","two"));
+        System.out.println(s.replace ("o","v"));
+    }
+}   
